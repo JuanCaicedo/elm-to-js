ed17f6f417cd8989f365c5c77b5e4d786406aec8
Juan Caicedo <juan@spanishdict.com>
2016-04-05 22:08:31 -0400

    two legislators (without map)

--- main.js
+++ main.js
@@ -7,22 +7,39 @@ var div   = hh.div;
 var span  = hh.span;
 var h1    = hh.h1;
 var table = hh.table;
+var tbody = hh.tbody;
 var tr    = hh.tr;
 var td    = hh.td;
 
 var initialState =  {
-  firstName: 'Juan',
-  lastName: 'Caicedo'
+  legislators: [{
+    firstName: 'Juan',
+    lastName: 'Caicedo'
+  }, {
+    firstName: 'Carson',
+    lastName: 'Banov'
+  }]
 };
 var loop = main(initialState, render, vdom)
 
 document.querySelector('#content').appendChild(loop.target)
 
-function render(state) {
+function legislatorTable(legislators) {
   return table('.table.table-striped', [
-    tr([
-      td(state.firstName),
-      td(state.lastName)
+    tbody([
+      tr([
+        td(legislators[0].firstName),
+        td(legislators[0].lastName)
+      ]),
+      tr([
+        td(legislators[1].firstName),
+        td(legislators[1].lastName)
+      ])
     ])
   ]);
+
+}
+
+function render(state) {
+  return div(legislatorTable(state.legislators));
 }
