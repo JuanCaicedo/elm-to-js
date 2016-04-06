069b4795a7f174ef444ad5b3733e450386806778
Juan Caicedo <juan@spanishdict.com>
2016-04-05 21:30:48 -0400

    first name last name

--- main.js
+++ main.js
@@ -3,17 +3,26 @@ var vh = require('virtual-dom/h');
 var hh = require('hyperscript-helpers')(vh);
 var main = require('main-loop')
 
-var div  = hh.div;
-var span = hh.span;
-var h1   = hh.h1;
+var div   = hh.div;
+var span  = hh.span;
+var h1    = hh.h1;
+var table = hh.table;
+var tr    = hh.tr;
+var td    = hh.td;
 
 var initialState =  {
-  title: "Fantasy Legislature"
+  firstName: 'Juan',
+  lastName: 'Caicedo'
 };
 var loop = main(initialState, render, vdom)
 
 document.querySelector('#content').appendChild(loop.target)
 
 function render(state) {
-  return h1(state.title);
+  return table([
+    tr([
+      td(state.firstName),
+      td(state.lastName)
+    ])
+  ]);
 }
